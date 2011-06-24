treyhunner/django-moderation

commit 74e4ba9edda7ae57008224f7f5bf4bd357e7ecb1

Do not check for previous versions of newly created objects

src/moderation/register.py
@@ -140,6 +140,8 @@ class ModerationManager(object):
                 moderated_obj.save()
 
     def _get_unchanged_object(self, instance):
+        if pk is None:
+            return None
         pk = instance.pk
         try:
             unchanged_obj = instance.__class__._default_manager.get(pk=pk)

commit f0ba5f2bb136c1d146a74c99d96ddf9bd44ce6b5

Fix bug introduced by 74e4ba9edda7ae57008224f7f5bf4bd357e7ecb1

src/moderation/register.py
@@ -141,7 +141,7 @@ class ModerationManager(object):
                 moderated_obj.save()
 
     def _get_unchanged_object(self, instance):
-        if pk is None:
+        if instance.pk is None:
             return None
         pk = instance.pk
         try: